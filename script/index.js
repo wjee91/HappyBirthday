1e2c7c66bc3d7b8e17fa20aebb631e2e76c34f09
wjee0910 <wjee0910@gmail.com>
2019-10-13 21:33:53 -0700

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,4 +1,183 @@
 var STARTED = false, COUNT = 0, LIMIT = 999, TAU = Math.PI * 2;
+var divMeteor = document.getElementById("meteor");
+    divBase = document.getElementById("base"),
+    divNote = document.getElementById("note"),
+    divBundle = document.getElementById("bundle"),
+    divButton = document.getElementById("button"),
+    divPop = document.getElementById("pop"),
+    divBirthday = document.getElementById("birthday");
+var balloons = ["balloon1", "balloon2", "balloon3", "balloon4", "balloon5"];
+
+// Meteor
+var context;
+var arr = new Array();
+var stellarCount = 800;
+var meteors = new Array();
+var meteorCount = 10;
+
+//初始化画布及context
+function initStellar() {
+    //获取canvas
+    windowWidth = window.innerWidth;
+    divMeteor.width = windowWidth;
+    divMeteor.height=window.innerHeight;
+    //获取context
+    context = divMeteor.getContext("2d");
+}
+
+//创建一个星星对象
+var Stellar = function () {
+    this.x = windowWidth * Math.random(); //横坐标
+    this.y = 5000 * Math.random(); //纵坐标
+    this.text="."; //文本
+    this.color = "#fff"; //颜色
+
+    //产生随机颜色
+    this.getColor=function () {
+        var _r = Math.random();
+
+        if (_r < 0.5) {
+            this.color = "#333";
+        }
+        else {
+            this.color = "#fff";
+        }
+    }
+
+    //初始化
+    this.init = function () {
+        this.getColor();
+    }
+
+    //绘制
+    this.draw = function () {
+        context.fillStyle = this.color;
+        context.fillText(this.text, this.x, this.y);
+    }
+}
+
+//星星闪起来
+function playStellar() {
+    for (var i = 0; i < stellarCount; i++){
+        arr[i].getColor();
+        arr[i].draw();
+    }
+
+    setTimeout("playStellar()", 100);
+}
+
+/*流星雨开始*/
+ var Meteor = function () {
+    this.x = -1;
+    this.y = -1;
+    this.length = -1; //长度
+    this.angle = 30; //倾斜角度
+    this.width = -1; //宽度
+    this.height = -1; //高度
+    this.speed = 1; //速度
+    this.offset_x = -1; //横轴移动偏移量
+    this.offset_y = -1; //纵轴移动偏移量
+    this.alpha = 1; //透明度
+    this.color1 = ""; //流星颜色
+    this.color2 = ""; //流星颜色
+
+    this.init = function () {
+        this.getPos();
+        this.alpha = 1; //透明度
+        this.getRandomColor();
+        //最小长度，最大长度
+        var x = Math.random() * 80 + 150;
+        this.length = Math.ceil(x);
+        //x = Math.random()*10+30;
+        this.angle = 30; //流星倾斜角
+        x = Math.random() + 0.5;
+        this.speed = Math.ceil(x); //流星的速度
+        var cos = Math.cos(this.angle * 3.14 / 180);
+        var sin = Math.sin(this.angle * 3.14 / 180);
+        this.width = this.length * cos; //流星所占宽度
+        this.height = this.length * sin; //流星所占高度
+        this.offset_x = this.speed * cos;
+        this.offset_y = this.speed * sin;
+    }
+
+    /**************获取随机颜色函数*****************/
+    this.getRandomColor = function () {
+        var a = Math.ceil(255 - 240 * Math.random());
+        //中段颜色
+        this.color1 = "rgba("+a+", "+a+", "+a+", 1)";
+        //结束颜色
+        this.color2 = "#111";
+    }
+
+     /***************重新计算流星坐标的函数******************/
+    this.countPos = function () {
+        //往左下移动,x减少，y增加
+        this.x = this.x - this.offset_x;
+        this.y = this.y + this.offset_y;
+    }
+
+    /*****************获取随机坐标的函数*****************/
+    this.getPos = function () {
+        //横坐标200-1200
+        this.x = Math.random() * window.innerWidth; //窗口高度
+        //纵坐标小于600
+        this.y = Math.random() * window.innerHeight; //窗口宽度
+    }
+
+     /****绘制流星***************************/
+    this.draw = function () {
+        context.save();
+        context.beginPath();
+        context.lineWidth = 1; //宽度
+        context.globalAlpha = this.alpha; //设置透明度
+
+        //创建横向渐变颜色,起点坐标至终点坐标
+        var line = context.createLinearGradient(this.x, this.y, this.x + this.width, this.y - this.height);
+
+        //分段设置颜色
+        line.addColorStop(0, "white");
+        line.addColorStop(0.3, this.color1);
+        line.addColorStop(0.6, this.color2);
+        context.strokeStyle = line;
+        //起点
+        context.moveTo(this.x, this.y);
+        //终点
+        context.lineTo(this.x + this.width, this.y - this.height);
+        context.closePath();
+        context.stroke();
+        context.restore();
+    }
+
+    this.move = function () {
+        //清空流星像素
+        var x = this.x + this.width - this.offset_x;
+        var y = this.y - this.height;
+        context.clearRect(x - 3, y - 3, this.offset_x + 5, this.offset_y + 5);
+        //context.strokeStyle = "red";
+        //context.strokeRect(x, y-1, this.offset_x + 1, this.offset_y + 1);
+        //重新计算位置，往左下移动
+        this.countPos();
+        //透明度增加
+        this.alpha -= 0.002;
+        //重绘
+        this.draw();
+    }
+}
+
+//绘制流星
+function playMeteors() {
+    for (var i = 0; i < meteorCount; i++) {
+        var meteor = meteors[i];
+        meteor.move();
+        if (meteor.y > window.innerHeight) {
+            context.clearRect(meteor.x, meteor.y - meteor.height, meteor.width, meteor.height);
+            meteors[i] = new Meteor();
+            meteors[i].init();
+        }
+    }
+
+    setTimeout("playMeteors()", 2);
+}
 
 // Stars
 var Particle = (function () {
@@ -92,7 +271,7 @@ var Stars = (function () {
         };
 
         this.buffer = document.createElement("canvas");
-        document.getElementById("base").appendChild(this.buffer);
+        divBase.appendChild(this.buffer);
         this.bufferContext = this.buffer.getContext("2d");
         this.supportsFilters = (typeof this.bufferContext.filter !== "undefined");
 
@@ -169,7 +348,49 @@ var Stars = (function () {
         this.particles.push(particle);
 
         if (STARTED && ++COUNT == LIMIT) {
-            console.log("haha");
+            explodeTimer = setInterval(function () {
+                if (STARTED) {
+                    STARTED = false;
+                    for (x of balloons) {
+                        $("#" + x).explode({
+                            maxWidth: 12,
+                            minWidth: 4,
+                            maxAngle: 360,
+                            radius: 480,
+                            explodeTime: 250,
+                            gravity: 10,
+                            groundDistance: 4096,
+                            canvas: true,
+                            round: true,
+                            recycle: false,
+                            release: false
+                        });
+                    }
+                }
+                else {
+                    playAudio(divPop, false);
+                    initStellar();
+                    //画星星
+                    for (var i = 0; i < stellarCount; i++) {
+                        var stellar = new Stellar();
+                        stellar.init();
+                        stellar.draw();
+                        arr.push(stellar);
+                    }
+
+                    //画流星
+                    for (var i=0;i<meteorCount;i++) {
+                        var meteor = new Meteor();
+                        meteor.init();
+                        meteor.draw();
+                        meteors.push(meteor);
+                    }
+
+                    playStellar();//绘制闪动的星星
+                    playMeteors();//绘制流星
+                    clearInterval(explodeTimer);
+                }
+            }, 250);
         }
 
         return this;
@@ -247,7 +468,7 @@ var stars = new Stars({
     frames: createFrames(5, 80, 80),
     width: 340,
     height: 544,
-    maxParticles: 800,
+    maxParticles: 400,
     backgroundColor: "#000",
     blendMode: "lighter",
     filterBlur: 50,
@@ -260,7 +481,7 @@ var stars = new Stars({
 var Balloon = function (id, settings) {
     var self = document.getElementById(id);
 
-	this.bundle = document.getElementById("bundle");
+	this.bundle = divBundle;
 	this.balloon = {
 		obj: self,
 		x: Math.ceil(Math.random() * (this.bundle.clientWidth - self.clientWidth - settings.margin * 2)) + settings.left + settings.margin,
@@ -304,12 +525,11 @@ function createObj(type, parent, id, src) {
 }
 
 function createBalloons(settings) {
-    var ids = ["balloon1", "balloon2", "balloon3", "balloon4", "balloon5"],
-        srcs = ["image/balloon_blue.png", "image/balloon_gold.png", "image/balloon_pink.png", "image/balloon_violet.png", "image/balloon_white.png"];
+    var srcs = ["image/balloon_blue.png", "image/balloon_gold.png", "image/balloon_pink.png", "image/balloon_violet.png", "image/balloon_white.png"];
 
-    for (var i = 0, n = ids.length, m = srcs.length; i < n; i++) {
-        createObj("img", "bundle", ids[i], srcs[i % m]);
-        new Balloon(ids[i], settings);
+    for (var i = 0, n = balloons.length, m = srcs.length; i < n; i++) {
+        createObj("img", "bundle", balloons[i], srcs[i % m]);
+        new Balloon(balloons[i], settings);
     }
 }
 
@@ -397,12 +617,6 @@ function main() {
         width: 340,
         height: 544
     };
-
-    var divBase = document.getElementById("base"),
-        divNote = document.getElementById("note"),
-        divButton = document.getElementById("button"),
-        divPop = document.getElementById("pop"),
-        divBirthday = document.getElementById("birthday");
 
     var isClicked = false, isPlayed = false;
 
